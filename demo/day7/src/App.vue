d6abb15a1ca61f9fb4e9d10a86fb9dbddd6ce5cf
qq <920191277@qq.com>
2019-05-17 10:10:32 +0800

    子传父

---
+++ demo/day7/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <div class="header">
+      
+        <span  v-for="(item,index) in tabList" :key="index">{{item.title}}</span>
+   
+    </div>
+
+  <div class="cont">
+    <HelloWorld v-for="(item,index) in tabList" :key="index" :title="item.title" :cont="item.cont" @clickTabFn="tabFn"/>
+  </div>
+    
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      tabList:[
+            {
+              title:"周一",
+              cont:"今天是周一",
+            },
+            {
+              title:"周二",
+              cont:"今天是周二",
+            },
+            {
+              title:"周三",
+              cont:"今天是周三",
+            }
+          ]
+    }
+   
+  },
+  methods:{
+    tabFn(title){
+      console.log(title)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ demo/day7/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="box2">
+      <!-- <p>{{title}}</p> -->
+      <p  @click="blockfn">{{cont}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  props:["title","cont"],
+  methods:{
+    blockfn(){
+      this.$emit("clickTabFn",this.title)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
